29c72664b5dacb84f230b0b37e0f87cf26b4158a
Matthew Lee <leematthewrobert@yahoo.com.au>
2018-12-09 07:45:49 -0700

    Add server/refactor

---
+++ server/models/todo.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minLength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+
+// var otherTodo = new Todo({
+//   text: 'Something to do'
+//   });
+//
+// otherTodo.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e);
+// });
+
+module.exports = {
+  Todo
+}
---
+++ server/models/user.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    minLength: 1
+  }
+});
+
+// var user = new User({
+//   email: 'foo@bar.com'
+// });
+//
+// user.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save user', e);
+// });
+
+module.exports = {
+  User
+}
