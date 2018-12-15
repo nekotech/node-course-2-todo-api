d488b3c0e60cc9088e0c7af5c1ecd7b6cb9e70dc
Matthew Lee <leematthewrobert@yahoo.com.au>
2018-12-14 17:10:16 -0700

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,3 +96,41 @@ describe('GET /todods/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toBe(null);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexid = new ObjectID().toHexString();
+    request(app)
+      .delete(`/todos/${hexid}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object ID is invalid', (done) => {
+    var hexid = new ObjectID().toHexString();
+    request(app)
+      .delete('/todos/12345')
+      .expect(404)
+      .end(done);
+  });
+});
