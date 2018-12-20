cc5205479e9af909b8e5e3776749bb1d13c1e982
Matthew Lee <leematthewrobert@yahoo.com.au>
2018-12-19 20:29:38 -0700

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -9,7 +9,7 @@ var UserSchema = new mongoose.Schema({
     type: String,
     trim: true,
     required: true,
-    minLength: 1,
+    minlength: 1,
     unique: true,
     validate: {
       validator: validator.isEmail,
@@ -65,6 +65,25 @@ UserSchema.statics.findByToken = function (token) {
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
+  });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
   });
 };
 
